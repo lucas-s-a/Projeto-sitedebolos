4bb930bdd57d0f76e66196c711040fcca30eef3b
lucas-s-a <113945158+lucas-s-a@users.noreply.github.com>
2022-09-29 13:37:14 -0300

    Update contato.js

--- js/contato.js
+++ js/contato.js
@@ -1,57 +1,88 @@
-var form = document.getElementById('form')
-var nome = document.getElementById('nome')
-var email = document.getElementById('email')
-var telefone = document.getElementById('telefone')
-var mensagem = document.getElementById('mensagem')
-
-var nomeValue = nome.value;
-var emailValue = email.value;
-var telefoneValue = telefone.value;
-var mensagemValue = mensagem.value;
-
-function checkInputs() {
-
-  if(nomeValue == '') {
+function validados(){
+  var nome = document.getElementById('nome').value
+  var email = document.getElementById('email').value
+  var telefone = document.getElementById('telefone').value
+  var mensagem = document.getElementById('mensagem').value
+  if(nome == '') {
     // mostrar erro
-    erro1.innerHTML = "Preencha esse campo.";
-    document.getElementById('nome').style.borderColor= "#e74c3c"
-  } else {
-    // adicionar borda verde
-    document.getElementById('email').style.borderColor= "#2ecc71"
-    
-  }
-  
-  if(emailValue == '') {
+    erro1.innerHTML = "Preencha esse campo."
+    // document.getElementById('nome').style.borderColor= "#e74c3c"
+  }else{
+    erro1.innerHTML = ""
+  }}
+  /*}else if (email == '') {
     // mostrar erro
     erro2.innerHTML = "Preencha esse campo."
-    document.getElementById('erro1').style.color= "#e74c3c"
-    document.getElementById('email').style.borderColor= "#e74c3c"
-  } else {
-    // adicionar borda verde
-    document.getElementById('email').style.borderColor= "#2ecc71"
-    
-  }
-  if(telefoneValue == '') {
-    // mostrar erro
+  //  document.getElementById('erro1').style.color= "#e74c3c"
+  //  document.getElementById('email').style.borderColor= "#e74c3c"
+  }else if(telefone == ''){
+  // mostrar erro
     erro3.innerHTML = "Preencha esse campo."
-    document.getElementById('telefone').style.borderColor= "#e74c3c"
-  } else {
-    // adicionar borda verde
-    document.getElementById('telefone').style.borderColor= "#2ecc71"
-    
-    
-  }
-  
-  if(mensagemValue == '') {
+  //  document.getElementById('telefone').style.borderColor= "#e74c3c"
+
+  } if(mensagem == ''){
     // mostrar erro
     erro4.innerHTML = "Preencha esse campo."
-    document.getElementById('mensagem').style.borderColor= "#e74c3c"
-  } else {
-    // adicionar borda verde
-    document.getElementById('mensagem').style.borderColor= "#2ecc71"
-    
+  //document.getElementById('mensagem').style.borderColor= "#e74c3c"
+  }*/
+function validados1(){
+  var nome = document.getElementById('nome').value
+  var email = document.getElementById('email').value
+  var telefone = document.getElementById('telefone').value
+  var mensagem = document.getElementById('mensagem').value
+  if (email == '') {
+    // mostrar erro
+    erro2.innerHTML = "Preencha esse campo."
+  }else{
+    erro2.innerHTML = ""
+  }
+}
+function validados2(){
+  var nome = document.getElementById('nome').value
+  var email = document.getElementById('email').value
+  var telefone = document.getElementById('telefone').value
+  var mensagem = document.getElementById('mensagem').value
+  if(telefone == ''){
+  // mostrar erro
+    erro3.innerHTML = "Preencha esse campo."
+  //  document.getElementById('telefone').style.borderColor= "#e74c3c"
+  }else{
+    erro3.innerHTML = ""
+  }
+}
+
+function validados3(){
+  var nome = document.getElementById('nome').value
+  var email = document.getElementById('email').value
+  var telefone = document.getElementById('telefone').value
+  var mensagem = document.getElementById('mensagem').value
+  if(mensagem == ''){
+    // mostrar erro
+    erro4.innerHTML = "Preencha esse campo."
+  //document.getElementById('mensagem').style.borderColor= "#e74c3c"
+  }else{
+    erro4.innerHTML = ""
+  }
 }
 
 
-}
-cliquebotao.addEventListener('click', checkInputs)
+res.addEventListener('click', validados)
+res.addEventListener('click', validados1)
+res.addEventListener('click', validados2)
+res.addEventListener('click', validados3)
+
+//} else if (telefone !== '') {
+  // adicionar borda verde
+//document.getElementById('telefone').style.borderColor= "#2ecc71" 
+
+//}else if (email !== ''){
+  // adicionar borda verde
+//document.getElementById('email').style.borderColor= "#2ecc71"
+
+ //}else if (nome !== ''){
+  // adicionar borda verde
+ // document.getElementById('nome').style.borderColor= "#2ecc71" 
+
+ //} else if (mensagem !== ''){
+  // adicionar borda verde
+ // document.getElementById('mensagem').style.borderColor= "#2ecc71"
